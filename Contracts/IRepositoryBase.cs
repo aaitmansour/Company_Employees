56afd3829767d524da32b1dc9ca02893382054d9
aaitmansour <ayoub.atm000@gmail.com>
2022-10-07 12:57:53 +0100

    new folder contracts

---
+++ Contracts/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll(bool trackchanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackchanges);
+
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
