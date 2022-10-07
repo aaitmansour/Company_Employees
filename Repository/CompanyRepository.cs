51bf2954470e4e1293b599b97a7620528dbd77d4
aaitmansour <ayoub.atm000@gmail.com>
2022-10-07 16:19:35 +0100

    new oooooo

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Modeles;
+using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+
+        {
+        }
+    }
+}
