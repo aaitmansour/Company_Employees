9610a78fc627ef0332c7bde3d347699dea218801
aaitmansour <ayoub.atm000@gmail.com>
2022-10-08 23:47:23 +0100

    new

--- Company_Employees/Program.cs
+++ Company_Employees/Program.cs
@@ -1,12 +1,13 @@
+using NLog;
 var builder = WebApplication.CreateBuilder(args);
-
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//builder.Services.ConfigureLoggerService();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
