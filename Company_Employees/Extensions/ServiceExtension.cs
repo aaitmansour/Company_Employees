b29ba7cf1b29e0eb61487bb624b2a1ae40d3ae68
aaitmansour <ayoub.atm000@gmail.com>
2022-10-11 10:28:46 +0100

    Adding services

---
+++ Company_Employees/Extensions/ServiceExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Company_Employees.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(Options =>
+            {
+
+            });
+    }
+}
--- Company_Employees/Program.cs
+++ Company_Employees/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //builder.Services.ConfigureLoggerService();
+//builder.Services.ConfigureRepositoryManager();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
