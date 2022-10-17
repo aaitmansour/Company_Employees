74852d0bdde093e347b0340c35df979aae79e619
aaitmansour <ayoub.atm000@gmail.com>
2022-10-17 16:25:36 +0100

    Telling the server to respect the accept header

--- Company_Employees/Program.cs
+++ Company_Employees/Program.cs
@@ -15,7 +15,10 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddControllers()
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+}).AddXmlDataContractSerializerFormatters()
     .AddApplicationPart(typeof(CompanyEmployee.Presentation.AssemplyReference).Assembly);
 
 
