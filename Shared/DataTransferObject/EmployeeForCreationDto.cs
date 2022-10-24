16179ab709f73fd1ff0e5740457bd5fab1532824
aaitmansour <ayoub.atm000@gmail.com>
2022-10-24 17:39:03 +0100

    positional records start losing readability once the attributes are applied, and for that reason, we like using init setters if we have to apply validation attributes.

--- Shared/DataTransferObject/EmployeeForCreationDto.cs
+++ Shared/DataTransferObject/EmployeeForCreationDto.cs
@@ -7,13 +7,17 @@ using System.Threading.Tasks;
 
 namespace Shared.DataTransferObject
 {
-    public record EmployeeForCreationDto([Required(ErrorMessage ="Employee name is required field.")]
-    [MaxLength(30, ErrorMessage ="Maximum length for the Name is 30 characters.")]
-    string Name,
-    [Required(ErrorMessage ="Age is a required field")]
-    int Age,
-    [Required(ErrorMessage ="Position is a required field")]
-    [MaxLength(20, ErrorMessage ="Maximum length for the Position is 20 characters.")]
-    string Position);
-    
+    public record EmployeeForCreationDto
+    {
+        [Required(ErrorMessage = "Employee name is required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Age is a required field")]
+        public int Age { get; init; }
+
+        [Required(ErrorMessage = "Position is a required field")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string? Position { get; init; }
+    }
 }
